3a44b42f1b0236392d347ab415940eaed75c8ea3
DarulCode <ahmaddarul72@gmail.com>
2024-11-17 16:30:01 +0700

    init: initialize project

---
+++ src/test/java/git/darul/mandiritest/MandiriTestApplicationTests.java
@@ -0,0 +1,13 @@
+package git.darul.mandiritest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandiriTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
