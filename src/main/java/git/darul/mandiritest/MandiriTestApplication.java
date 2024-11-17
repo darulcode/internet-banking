714bf0aabbe2f5acb1282c696da0e1023f702bcf
DarulCode <ahmaddarul72@gmail.com>
2024-11-17 16:26:26 +0700

    init: initialize project

---
+++ src/main/java/git/darul/mandiritest/MandiriTestApplication.java
@@ -0,0 +1,15 @@
+package git.darul.mandiritest;
+
+import jakarta.persistence.EntityListeners;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class MandiriTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MandiriTestApplication.class, args);
+    }
+}
