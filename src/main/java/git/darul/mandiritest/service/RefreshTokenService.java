fc1c174dab64aac3887d2c9ced68597f0ea62579
DarulCode <ahmaddarul72@gmail.com>
2024-11-17 16:31:35 +0700

    feature: create refresh token service

---
+++ src/main/java/git/darul/mandiritest/service/RefreshTokenService.java
@@ -0,0 +1,8 @@
+package git.darul.mandiritest.service;
+
+public interface RefreshTokenService {
+    String createToken(String userId);
+    void deleteRefreshToken(String userId);
+    String rotateRefreshToken(String userId);
+    String getUserIdByToken(String token);
+}
