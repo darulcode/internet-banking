6fe7440b86276fc3ee5c97592638bdaa4f86e50a
DarulCode <ahmaddarul72@gmail.com>
2024-11-17 16:34:57 +0700

    feature: create custom auth entry point and custom access denied handler

---
+++ src/main/java/git/darul/mandiritest/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,33 @@
+package git.darul.mandiritest.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import git.darul.mandiritest.dto.response.CommonResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        CommonResponse<?> commonResponse = new CommonResponse<>(
+                HttpStatus.FORBIDDEN.value(),
+                accessDeniedException.getMessage(),
+                null
+        );
+        String responseString = objectMapper.writeValueAsString(commonResponse);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(responseString);
+    }
+}
