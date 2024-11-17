5f374564b95d70591117d8460e9b5a20135c2b8e
DarulCode <ahmaddarul72@gmail.com>
2024-11-17 16:31:18 +0700

    feature: create class constants

---
+++ src/main/java/git/darul/mandiritest/constants/Constants.java
@@ -0,0 +1,31 @@
+package git.darul.mandiritest.constants;
+
+public class Constants {
+
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
+
+    public static final String REFRESH_TOKEN_REQUIRED_MESSAGE="Refresh Token is required";
+
+
+
+    public static final String TABLE_USER = "m_user";
+    public static final String TABLE_ACCOUNT = "m_account";
+    public static final String TABLE_TRANSACTIONS = "t_transactions";
+
+
+    public static final String AUTH_API = "/api/auth";
+    public static final String ACCOUNT_API = "/api/account";
+    public static final String TRANSACTION_API = "/api/transaction";
+
+
+    public static final String SUCCESS_FETCH_ACCOUNT= "Successfully fetched account";
+    public static final String SUCCESS_FETCH_BALANCE= "Successfully fetched balance";
+    public static final String SUCCESS_FETCH_ALL_TRANSACTION= "Successfully fetched transaction";
+    public static final String SUCCESS_FETCH_TRANSACTION= "Successfully fetched transaction";
+    public static final String SUCCESS_UPDATE_BALANCE= "Successfully update balance";
+    public static final String SUCCESS_CREATE_TRANSACTION= "Transaction successfully";
+    public static final String NOTFOUND_TRANSACTION= "Transaction not found";
+    public static final String FAILED_FETCH_BALANCE= "Account Number Not Found";
+    public static final String NOTFOUND_BANK_ACCOUNT= "Bank account not found";
+    public static final String BALANCE_LOW= "Your Balance low";
+}
