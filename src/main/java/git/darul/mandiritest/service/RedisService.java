0fe7c200b4a1f1671268ca75d0bcd901104ee6dc
DarulCode <ahmaddarul72@gmail.com>
2024-11-17 16:31:45 +0700

    feature: create redis service

---
+++ src/main/java/git/darul/mandiritest/service/RedisService.java
@@ -0,0 +1,10 @@
+package git.darul.mandiritest.service;
+
+import java.time.Duration;
+
+public interface RedisService {
+    void save(String key, String value, Duration duration);
+    String get(String key);
+    void delete(String key);
+    Boolean isExist(String key);
+}
